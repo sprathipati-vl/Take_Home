e07de8122baf9795d13ddb25ff332f2cdb6d5c58
sprathipati-vl <sravan.prathipati@valuelabs.com>
2020-08-09 02:06:28 +0530

    New Commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {APIService} from './services/api.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'take-home';
+  serviceList: any;
+  providerList: any;
+  errorMessage : string;
+  selectedIndex: number;
+  constructor(private apiService : APIService)
+  {
+
+  }
+
+  ngOnInit()
+  {
+    this.getServices();
+    this.getProviders();
+  }
+  
+  getServices()
+  {
+    this.apiService.getServices()
+    .subscribe(
+      (response) => {                           
+        this.serviceList = response.data;
+      },
+      (error) => {                              
+        this.errorMessage = error;
+      }
+    )
+  }
+
+  getProviders()
+  {
+    this.apiService.getProviders()
+    .subscribe(
+      (response) => {                           
+        this.providerList = response.data;
+      },
+      (error) => {                              
+        this.errorMessage = error;
+      }
+    )
+  }
+
+  selectService(i: number)
+  {
+    this.selectedIndex = i;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { APIService } from './services/api.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [APIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
