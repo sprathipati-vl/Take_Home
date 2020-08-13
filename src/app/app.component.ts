fb4b46f11cd072a46ecb8649ef95dc9ac3e937c3
sprathipati-vl <sravan.prathipati@valuelabs.com>
2020-08-13 11:37:31 +0530

    Angular Changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit{
   title = 'take-home';
   serviceList: any;
   providerList: any;
+  serviceProviderList : any;
   errorMessage : string;
   selectedIndex: number;
   constructor(private apiService : APIService)
@@ -29,7 +30,9 @@ export class AppComponent implements OnInit{
     this.apiService.getServices()
     .subscribe(
       (response) => {                           
-        this.serviceList = response.data;
+        this.serviceList = response["data"];
+        if(this.serviceList)
+         this.filterServiceList(this.serviceList);
       },
       (error) => {                              
         this.errorMessage = error;
@@ -42,7 +45,9 @@ export class AppComponent implements OnInit{
     this.apiService.getProviders()
     .subscribe(
       (response) => {                           
-        this.providerList = response.data;
+        this.providerList = response;
+        if(this.providerList)
+         this.filterProviderList(this.providerList);
       },
       (error) => {                              
         this.errorMessage = error;
@@ -50,8 +55,49 @@ export class AppComponent implements OnInit{
     )
   }
 
-  selectService(i: number)
+  selectService(i: number, service : any)
   {
     this.selectedIndex = i;
+    this.filterServiceProviderList(service);
   }
+
+  filterServiceProviderList(service: any)
+  {
+    this.serviceProviderList = this.providerList.filter(x=> x["attributes"]["service"] == service["attributes"]["name"]);
+  }
+
+  filterServiceList(list: any)
+  {
+    let newArr = [];
+    list.forEach((item) => {
+        if (newArr.findIndex(i => i["attributes"]["name"] == item["attributes"]["name"]) === -1) 
+        {
+            newArr.push(item)
+        }    
+    });
+    this.serviceList = newArr;
+  }
+
+  filterProviderList(list : any)
+  {
+    let newArr = [];
+    list["included"].forEach((item, index) => {
+        if (item["attributes"]["name"] != "" && item["attributes"]["name"] != undefined) 
+        {
+          if(newArr.findIndex(i => i["attributes"]["name"] == item["attributes"]["name"]) === -1)
+          {
+            // Get Data Item.
+            let dataItem  = list["data"].find(x=> x?.relationships?.locations?.data[0].id == item.id);
+            if(dataItem)
+            item["pimg"] = dataItem?.attributes["profile-image"];
+            item["subspecialties"] = dataItem?.attributes?.subspecialties;
+            newArr.push(item)
+          }
+        }    
+    });
+    this.providerList = newArr;
+    this.serviceProviderList = newArr;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {CommonModule} from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { APIService } from './services/api.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProvidersComponent } from './providers/providers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvidersComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    CommonModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [APIService],
   bootstrap: [AppComponent]
